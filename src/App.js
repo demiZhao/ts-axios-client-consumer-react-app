62678856fabc45ddf20baa2a9de8b4b0cfbccc8c
demi zhao <demizhao@demis-MacBook-Pro.local>
2021-08-15 16:21:21 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { PetsApi } from 'PetStore';
+import { useState, useEffect } from "react"
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [petList, setPetList] = useState([]);
+
+  useEffect(async () => {
+    try {
+      new PetsApi()
+        .listPets()
+        .then((response) =>
+          setPetList(response.data.map((pet) => JSON.stringify(pet)))
+        );
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{petList}</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
